38e9e6d16f7d1f3786521d65b6cf173f82102b41
crossroads30 <crossroads30@mail.ru>
2024-01-07 18:45:29 +0300

    feat: add initial state, add import action

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,16 @@
 import React, { useContext, useReducer } from 'react'
+import { TOGGLE_AMOUNT } from './actions'
 
 const AppContext = React.createContext()
 
+const initialState = {
+	total: 0,
+	amount: 0,
+}
+
 const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer()
+
 	return <AppContext.Provider value={'hello'}>{children}</AppContext.Provider>
 }
 
