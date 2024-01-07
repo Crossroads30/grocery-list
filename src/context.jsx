d30739b4e331b74fef63869a0257fe738a45b718
crossroads30 <crossroads30@mail.ru>
2024-01-07 19:23:24 +0300

    feat: add getLocalStorage func

--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,50 @@
-import React, { useContext, useReducer } from 'react'
-import { TOGGLE_AMOUNT } from './actions'
+import React, { useContext, useEffect, useReducer } from 'react'
+import { TOGGLE_AMOUNT, DISPLAY_ITEMS } from './actions'
+import reducer from './reducer'
 
 const AppContext = React.createContext()
 
+  const getLocalStorage = () => {
+		let list = localStorage.getItem('list')
+		if (list) {
+			return (list = JSON.parse(localStorage.getItem('list')))
+		} else {
+			return []
+		}
+	}
+
 const initialState = {
+  list: [],
 	total: 0,
 	amount: 0,
 }
 
 const AppProvider = ({ children }) => {
-	const [state, dispatch] = useReducer()
+	const [state, dispatch] = useReducer(reducer, initialState)
 
-	return <AppContext.Provider value={'hello'}>{children}</AppContext.Provider>
+  console.log(state)
+
+  	const toggleAmount = (id, type) => {
+			dispatch({ type: TOGGLE_AMOUNT, payload: { id, type } })
+		}
+
+    const getList = () => {
+      const list = getLocalStorage()
+      dispatch({ type: DISPLAY_ITEMS, payload: list })
+    }
+    
+    useEffect(() => {
+      getList()
+    }, [])
+
+    // useEffect(() => {
+    //   localStorage.setItem('list', JSON.stringify(state.list)) 
+    // }, [state.list])
+
+
+	return (
+		<AppContext.Provider value={toggleAmount}>{children}</AppContext.Provider>
+	)
 }
 
 export const useGlobalContext = () => {
