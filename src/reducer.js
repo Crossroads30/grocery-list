5c6cf49d4dfed84d59f51f21f89dd9571d6654fd
crossroads30 <crossroads30@mail.ru>
2024-01-07 22:06:10 +0300

    feat: add action for addItem, showItemAlert

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,29 @@
-import { DISPLAY_ITEMS, TOGGLE_AMOUNT } from "./actions"
-
+import { DISPLAY_ITEMS, TOGGLE_AMOUNT, ADD_ITEM, SHOW_ALERT } from './actions'
 
 const reducer = (state, action) => {
-if (action.type === DISPLAY_ITEMS) {
-  return {
-    ...state,
-    list: action.payload
-  }
-}
+	if (action.type === ADD_ITEM) {
+		const newItem = {
+			id: new Date().getTime().toString(),
+			title: action.payload,
+			cost: '?',
+		}
+		return {
+			...state,
+			itemList: [...state.itemList, newItem],
+			alert: {
+				show: true,
+				msg: 'продукт добавлен!',
+				type: 'success',
+			},
+		}
+	}
+	if (action.type === SHOW_ALERT) {
+		return {
+			...state,
+			alert: { show: false, msg: '', type: '' },
+		}
+	}
+	return state
 }
 
 export default reducer
